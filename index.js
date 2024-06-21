ff58986921f0de3d39ee740a9c9c384d2e9b81a7
mariobac1 <mario.moralesbac1@gmail.com>
2024-06-20 20:46:18 -0400

    Se agregael path, para que no de errores

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require( 'path'  );
+
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
@@ -21,6 +23,10 @@ app.use( express.json());
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res ) => {
+  res.sendFile( path.join( __dirname, 'public/index.html' ) );
+});
 
 // TODO: CRUD: Eventos
 
